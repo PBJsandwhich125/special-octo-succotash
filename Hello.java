e3d2f5721971d2df393f6ed609810b9f5e868c4b
PBJ <pbjsandwhich80@gmail.com>
2021-10-21 13:23:06 -0700

    Hello World for Java

---
+++ Hello.java
@@ -0,0 +1,15 @@
+public class Hello {
+    public static void main(String[] arg) {
+        System.out.println("Hello World!");
+
+        int firstNumber = 10;
+        int secondNumber = 2;
+        int thirdNumber = 2*24;
+        int myTotal = firstNumber + secondNumber + thirdNumber;
+
+
+        System.out.println(myTotal);
+        int lastOne = 1000 - myTotal;
+        System.out.println(lastOne);
+    }
+}
